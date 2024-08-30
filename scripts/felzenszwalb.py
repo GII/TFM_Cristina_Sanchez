4f0a78d875155c89e8fb414ff5626cae046807f1
Cristina Sanchez <sanchezprado.13@gmail.com>
2024-08-30 10:53:21 +0200

    scripts pruebas

--- scripts/felzenszwalb.py
+++ scripts/felzenszwalb.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import time
 import numpy as np
 from skimage import io, color
 from skimage.measure import regionprops
@@ -11,21 +12,23 @@ import matplotlib.pyplot as plt
 HOME = os.getcwd()
 
 # Cargar la imagen
-IMAGES_PATH = os.path.join(HOME, "images", "rosa")
+IMAGES_PATH = os.path.join(HOME, "images", "nuevas")
 for img in Path(IMAGES_PATH).iterdir():
     IMAGE_PATH = os.path.join(IMAGES_PATH, img)
     image = io.imread(IMAGE_PATH)
-    
-    # Preprocesamiento
-    print("1. Suavizado Gaussiano")
-    image = cv2.GaussianBlur(image, (5, 5), 0)
 
     print("Aplicar la segmentación de Felzenszwalb")
-    segments_fz = felzenszwalb(image, scale=700, sigma=0.95, min_size=350)
+    start_time = time.time()
+    segments_fz = felzenszwalb(image, scale=600, sigma=0.75, min_size=100)
+    stop_time= time.time()
+    print("Tiempo de procesamiento:", stop_time-start_time)
 
     # Obtener el número de segmentos únicos
     num_segments = segments_fz.max() + 1
     print(num_segments)
+    areas = sorted([np.sum(segments_fz == segment_id) for segment_id in range(num_segments) if np.sum(segments_fz == segment_id)])
+    print("areas", len(areas), areas)
+    
     # Crear una imagen en blanco para colorear las regiones
     colored_segments = np.zeros_like(image)
 
@@ -33,8 +36,6 @@ for img in Path(IMAGES_PATH).iterdir():
     for segment_id in range(num_segments):
         # Crear un color aleatorio
         color_random = np.random.randint(0, 255, size=3)
-        
-        # Colorear el segmento con el color aleatorio
         colored_segments[segments_fz == segment_id] = color_random
 
     # Mostrar la imagen original y la segmentada lado a lado
